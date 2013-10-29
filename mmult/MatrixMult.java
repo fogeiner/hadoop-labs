3b51d1c623005b9c4d4ce4f8df6b179ebfd34746
Valentin Sviridov <fogeiner@gmail.com>
2013-10-29 21:46:05 +0100

    Fixed formatting

--- mmult/MatrixMult.java
+++ mmult/MatrixMult.java
@@ -16,103 +16,103 @@ import org.apache.hadoop.util.*;
 
 public class MatrixMult {
 
-    private static Logger LOGGER = Logger.getLogger(MatrixMult.class.getName());
+	private static Logger LOGGER = Logger.getLogger(MatrixMult.class.getName());
 
-    public static class Map extends MapReduceBase implements
-            Mapper<LongWritable, Text, IntWritable, MapWritable> {
+	private static List<Float> vector;
 
-        public void map(LongWritable key, Text value,
-                OutputCollector<IntWritable, MapWritable> output,
-                Reporter reporter) throws IOException {
+	public static void setVector(List<Float> vector) {
+		MatrixMult.vector = vector;
+	}
 
-            String tokens[] = value.toString().split("\\s+");
+	public static List<Float> getVector() {
+		return vector;
+	}
+	public static class Map extends MapReduceBase implements
+			Mapper<LongWritable, Text, IntWritable, MapWritable> {
 
-            IntWritable i = new IntWritable(Integer.parseInt(tokens[0]));
-            IntWritable j = new IntWritable(Integer.parseInt(tokens[1]));
-            FloatWritable Aij = new FloatWritable(Float.parseFloat(tokens[2]));
+		public void map(LongWritable key, Text value,
+				OutputCollector<IntWritable, MapWritable> output,
+				Reporter reporter) throws IOException {
 
-            MapWritable map = new MapWritable();
-            map.put(j, Aij);
+			String tokens[] = value.toString().split("\\s+");
 
-            LOGGER.info("i=" + i.get() + " j=" + j.get() + " Aij=" + Aij);
+			IntWritable i = new IntWritable(Integer.parseInt(tokens[0]));
+			IntWritable j = new IntWritable(Integer.parseInt(tokens[1]));
+			FloatWritable Aij = new FloatWritable(Float.parseFloat(tokens[2]));
 
-            output.collect(i, map);
-        }
-    }
+			MapWritable map = new MapWritable();
+			map.put(j, Aij);
 
-    public static class Reduce extends MapReduceBase implements
-            Reducer<IntWritable, MapWritable, IntWritable, FloatWritable> {
+			LOGGER.info("i=" + i.get() + " j=" + j.get() + " Aij=" + Aij);
 
-        public void reduce(IntWritable key, Iterator<MapWritable> values,
-                OutputCollector<IntWritable, FloatWritable> output,
-                Reporter reporter) throws IOException {
-            
-        	float product = 0;
-            List<Float> vector = getVector();
-            
-            while (values.hasNext()) {
-                MapWritable m = values.next();
-                
-                IntWritable jW = (IntWritable)m.keySet().iterator().next();
-                FloatWritable valueW = (FloatWritable)m.get(jW);
-                
-                int i = key.get();
-                int j = jW.get();
-                LOGGER.info("i="+i+" j="+j + " value=+"
-                		+vector.get(j - 1)+"*"+valueW.get());
-                product += vector.get(j - 1) * valueW.get();                
-            }
+			output.collect(i, map);
+		}
+	}
 
-            LOGGER.info("i=" + key.get() + " product=" + product);
+	public static class Reduce extends MapReduceBase implements
+			Reducer<IntWritable, MapWritable, IntWritable, FloatWritable> {
 
-            output.collect(key, new FloatWritable(product));
-        }
-    }
+		public void reduce(IntWritable key, Iterator<MapWritable> values,
+				OutputCollector<IntWritable, FloatWritable> output,
+				Reporter reporter) throws IOException {
 
-    private static List<Float> vector;
+			float product = 0;
+			List<Float> vector = getVector();
 
-    public static void setVector(List<Float> vector) {
-        MatrixMult.vector = vector;
-    }
+			while (values.hasNext()) {
+				MapWritable m = values.next();
 
-    public static List<Float> getVector() {
-        return vector;
-    }
+				IntWritable jW = (IntWritable) m.keySet().iterator().next();
+				FloatWritable valueW = (FloatWritable) m.get(jW);
 
-    public static void main(String[] args) throws Exception {
+				int i = key.get();
+				int j = jW.get();
+				LOGGER.info("i=" + i + " j=" + j + " value=+"
+						+ vector.get(j - 1) + "*" + valueW.get());
+				product += vector.get(j - 1) * valueW.get();
+			}
 
-        JobConf conf = new JobConf(MatrixMult.class);
+			LOGGER.info("i=" + key.get() + " product=" + product);
 
-        if (args.length != 3 || args[2] == null) {
-            System.err.println("The \"vector\" (3rd argument) is not defined");
-            System.out.println("Usage: " + args[0] + " input_dir output_dir vector");
-            return;
-        }
-
-        List<Float> vector = new ArrayList<Float>();
-        for (String token : args[2].split(" ")) {
-            vector.add(Float.parseFloat(token));
-        }
-        
-        MatrixMult.setVector(vector);
-
-        conf.setJobName("mmult");
-
-        conf.setInputFormat(TextInputFormat.class);
-        conf.setOutputFormat(TextOutputFormat.class);
-
-        conf.setMapOutputKeyClass(IntWritable.class);
-        conf.setMapOutputValueClass(MapWritable.class);
-        conf.setOutputKeyClass(IntWritable.class);
-        conf.setOutputValueClass(FloatWritable.class);
-
-        conf.setMapperClass(Map.class);
-        conf.setReducerClass(Reduce.class);
+			output.collect(key, new FloatWritable(product));
+		}
+	}
 
 
-        FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+	public static void main(String[] args) throws Exception {
 
-        JobClient.runJob(conf);
-    }
+		JobConf conf = new JobConf(MatrixMult.class);
+
+		if (args.length != 3 || args[2] == null) {
+			System.err.println("The \"vector\" (3rd argument) is not defined");
+			System.out.println("Usage: " + args[0]
+					+ " input_dir output_dir vector");
+			return;
+		}
+
+		List<Float> vector = new ArrayList<Float>();
+		for (String token : args[2].split(" ")) {
+			vector.add(Float.parseFloat(token));
+		}
+
+		MatrixMult.setVector(vector);
+
+		conf.setJobName("mmult");
+
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		conf.setMapOutputKeyClass(IntWritable.class);
+		conf.setMapOutputValueClass(MapWritable.class);
+		conf.setOutputKeyClass(IntWritable.class);
+		conf.setOutputValueClass(FloatWritable.class);
+
+		conf.setMapperClass(Map.class);
+		conf.setReducerClass(Reduce.class);
+
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+		JobClient.runJob(conf);
+	}
 }
